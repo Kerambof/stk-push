997b8df1d918b0044b258c1aab8d967ee0f194f9
Kerambof <kerambofrancis7@gmail.com>
2026-07-09 16:39:19 +0300

    Added callback endpoint

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 
 from mpesa import stk_push
 
@@ -7,7 +7,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-
     return render_template("index.html")
 
 
@@ -15,7 +14,6 @@ def home():
 def pay():
 
     phone = request.form["phone"]
-
     amount = request.form["amount"]
 
     response = stk_push(phone, amount)
@@ -24,6 +22,22 @@ def pay():
         "response.html",
         response=response
     )
+
+
+@app.route("/mpesa/callback", methods=["POST"])
+def mpesa_callback():
+
+    data = request.get_json()
+
+    print("=" * 60)
+    print("CALLBACK RECEIVED")
+    print(data)
+    print("=" * 60)
+
+    return jsonify({
+        "ResultCode": 0,
+        "ResultDesc": "Accepted"
+    })
 
 
 if __name__ == "__main__":
